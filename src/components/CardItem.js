438da37d91347cb6e5b6bc2aa8466fc318e2164f
tashawan23 <tas.wan23@gmail.com>
2021-02-20 15:21:54 +0800

    Edit css

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -3,12 +3,16 @@ import React from 'react'
 export const CardItem = props => {
     return (
         <div className="job-card">
+            <span className= "job-logo-wrapper">
             <img className="job-logo"
             alt="Company logo"
             src={props.src}>
             </img>
+            </span>
+            <span className="job-details">
             <div className="job-company">{props.name}</div>
             <div className="job-description">{props.text}</div>
+            </span>
             <div className="job-footer">
                 <span className="job-card-time">{props.timestamp}</span>
                 <span className="job-card-category">{props.category}</span>
